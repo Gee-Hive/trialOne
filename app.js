9b9b1aec3b3d1bed13a64597e7bf5a43c1053274
Ogunleye Gbenga <gbengaogunleye34@gmail.com>
2021-01-24 23:11:15 +0100

    Make a switch statement

---
+++ app.js
@@ -0,0 +1,18 @@
+attendanceSheet = ["James","John","Caroline","Janet","Shade","Tope","Kate"]
+
+function rowCall(attendanceSheet){
+    switch(attendanceSheet){
+        case "code11":
+            return attendanceSheet[0]
+            break;
+        case "code12":
+            return attendanceSheet[1]
+            break;
+        
+    }
+
+    console.log(rowCall("Code11"))
+
+}
+console.log("??")
+rowCall();
